3e9dab72625d537557c4a7c118f0c77dbd52b469
younes <elidrissiyazami.y@husky.neu.edu>
2020-03-16 00:27:21 -0400

    initial bar chart component

---
+++ js/barChart.js
@@ -0,0 +1,103 @@
+function barChart() {
+    let margin = {top: 80, right: 20, bottom: 20, left: 100},
+        width = 550,
+        height = 400;
+ 
+    function chart(selector, data) {
+        // Setup
+        const w = width - margin.left - margin.right,
+              h = height - margin.top - margin.bottom,
+              svg= d3.select(selector)
+                .append('svg')
+                .attr("width", width + margin.left  + margin.right )
+                .attr("height", height + margin.top  + margin.bottom )
+                .append('g')
+                .attr("transform",`translate(${margin.left},${margin.top})`);
+
+      
+        
+       //scales.
+
+       //extents; aka lowest and highest vals
+       const xMax = d3.max(data, d=>d.count);
+
+       const xScale =d3.scaleLinear([0,xMax],[0,width]);
+       const yScale=d3.scaleBand()
+            .domain(data.map(d=> d.fun_name))
+            .rangeRound([0,height])
+            .paddingInner(0.25);
+
+        //draw bars
+
+        const bars= svg.selectAll('.bar')
+            //join in data
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('class','bar')
+            //position
+            .attr('y', d=> yScale(d.fun_name))
+            .attr('width',d=> xScale(d.count))
+            .attr('height', yScale.bandwidth())
+            .style('fill','dodgerblue');
+
+
+        
+     
+        //Axes
+
+        //position of x axis
+        const xAxis = d3.axisTop(xScale)
+                        .tickFormat(formatTicks)
+                        .tickSizeInner(-height)
+                        .tickSizeOuter(0)
+        const xAxisDraw= svg.append('g')
+                        .attr('class','x axis')
+                        .call(xAxis);
+        //position of y axis
+        const yAxis = d3.axisLeft(yScale).tickSize(0);
+        const yAxisDraw= svg.append('g')
+                           .attr('class', 'y axis')
+                           .call(yAxis);
+
+        //space between y axis and labels(fun_name)
+        yAxisDraw.selectAll('text').attr('dx','-0.6em'); 
+
+        //headers ....
+        const header = svg.append('g')
+                        .attr('class','bar-header')
+                        //half the margin up before appending a text element
+                        .attr('transform', `translate(0,${-margin.top/2})`)
+                        .append('text');
+        //headline
+        header.append('tspan').text('bar chart (change title)');
+    }
+   //function for x axis format
+   function formatTicks(d){
+
+    return d3.format('')(d);
+
+
+}
+    chart.margin = function(_) {
+        if (!arguments.length) return margin;
+        margin = _;
+        return chart;
+    };
+
+    chart.width = function(_) {
+        if (!arguments.length) return width;
+        width = _;
+        return chart;
+    };
+
+    chart.height = function(_) {
+        if (!arguments.length) return height;
+        height = _;
+        return chart;
+    };
+
+    return chart;
+}
+
+  
--- js/visualization.js
+++ js/visualization.js
@@ -1,8 +1,72 @@
 ((() => {
+//Data util
 
-    typesOverview = typesOverviewChart();
+//if it is NA returns javascript undefined else returns the string
+const parseNA= string => (string ==='NA'? undefined: string);
+    
+    
+    //type conversion 
+function type(d){
+      return {
+          //string to int
+          count: +d.count,
+          fun_name: parseNA(d.fun_name),
+          arg_t1: parseNA(d.arg_t1),
+          arg_t2: parseNA(d.arg_t2),
+          arg_t3: parseNA(d.arg_t3),
+          arg_t4: parseNA(d.arg_t4)
+          
+ };  
+}
+//Data preparation
+function filterData(data){
+    return data.filter (d=>{
+        return (
+            d.count &&
+            d.fun_name
+        );
+    });
+}
+//prepare data for barChart
+function  prepareBarChart(data){
+    //d3 rollup returns a map
+    const dataMap=d3.rollup(
+        data,
+        //reducer function
+        v=> d3.sum(v,leaf=> leaf.count),
+        //groupBy
+        d=> d.fun_name
+    );
+     //converting the map to array
+    const dataArray= Array.from(dataMap, d=>({fun_name: d[0],count: d[1] }));
+    return dataArray;
+} 
 
-    d3.csv("data/tiny.csv")
-        .then((d) => typesOverview("#vis-svg-1", d));
+//Loading data
+d3.csv("data/tiny.csv",type)
+        .then(d => {
+          ready(d);
+        });
+
+//Charts reusable pattern
+
+//type chart 
+typesOverview = typesOverviewChart();
+
+//barchart 
+barchartVis=barChart();
+
+//Main function
+function ready(data){
+    //filtering data 
+    const dataClean=filterData(data);
+    const barChartData= prepareBarChart(dataClean).sort((a,b)=>{
+        return d3.descending(a.count-b.count);
+    });
+    //calling the vis
+    typesOverview("#vis-svg-1", data);
+    barchartVis("#barchart-1", barChartData);
+}
+
 
 })());
