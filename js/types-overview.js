cc5a694ab1485d98045d742a1137c34e2d497e7e
Cameron Moy <camoy@cs.umd.edu>
2020-03-17 15:44:17 -0400

    More comments

--- js/types-overview.js
+++ js/types-overview.js
@@ -1,3 +1,4 @@
+// Create types overview chart. This is the main view in the visualization.
 function typesOverviewChart() {
     let margin = {top: 20, right: 20, bottom: 20, left: 20},
         width = 550,
@@ -73,21 +74,28 @@ function typesOverviewChart() {
     return chart;
 }
 
+// These are the columns from the table that we will use for our visualization.
 const dataKeys = ["fun_name", "arg_t1", "arg_t2", "arg_t3", "arg_t4"];
 
+// Main function for transforming the input data into something that is usable
+// for the D3 Sankey library.
 function sankeyData(data) {
     return sankeyTreeToNodeLink(sankeyDataToTree(data));
 }
 
+// Convert the raw data from the CSV into a format into a tree.
 function sankeyDataToTree(data) {
     let root = { __value: 0 };
+
+    // For every row in the dataset.
     for (const d of data) {
         let cur = root;
         for (const k of dataKeys) {
+            // Don't generate a node for NA
             let v = d[k];
-            // TODO: Ok to ignore NA like this?
             if (!v || v === "NA") break;
 
+            // Get existing node or generate new one and add function count
             let next = cur[v];
             if (!next) next = cur[v] = { __value: 0 };
             next.__value += parseInt(d.count);
@@ -97,15 +105,19 @@ function sankeyDataToTree(data) {
     return root;
 }
 
+// Turn the tree data structure into a node and link dictionary that the library
+// understands.
 function sankeyTreeToNodeLink(root) {
     let vs = [];
     let vsIndex = {};
     let es = [];
     let queue = [{ name: undefined, data: root, depth: 0 }];
 
+    // Do a BFS over the tree to get flows connected correctly
     while (queue.length !== 0) {
         let cur = queue.shift();
         for (const k in cur.data) {
+            // The metadata __value is not a child
             if (k === "__value") continue;
             let d = cur.depth + 1,
                 v = { name: k, data: cur.data[k], depth: d },
