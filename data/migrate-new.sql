0e2151e0a2d1ef25532504ad319c460430e00cf2
Julia Belyakova <julbinb@gmail.com>
2020-04-08 19:00:49 -0400

    new database design

---
+++ data/migrate-new.sql
@@ -0,0 +1,162 @@
+--
+-- types table
+--
+
+CREATE TABLE types(
+  package_being_analyzed TEXT,
+  package TEXT,
+  fun_name TEXT,
+  fun_id TEXT,
+  dispatch TEXT,
+  arg_t_r TEXT,
+  arg_c_r TEXT,
+  arg_a_r TEXT,
+  arg_t0 TEXT,
+  arg_c0 TEXT,
+  arg_a0 TEXT,
+  arg_t1 TEXT,
+  arg_c1 TEXT,
+  arg_a1 TEXT,
+  arg_t2 TEXT,
+  arg_c2 TEXT,
+  arg_a2 TEXT,
+  arg_t3 TEXT,
+  arg_c3 TEXT,
+  arg_a3 TEXT,
+  arg_t4 TEXT,
+  arg_c4 TEXT,
+  arg_a4 TEXT,
+  arg_t5 TEXT,
+  arg_c5 TEXT,
+  arg_a5 TEXT,
+  arg_t6 TEXT,
+  arg_c6 TEXT,
+  arg_a6 TEXT,
+  arg_t7 TEXT,
+  arg_c7 TEXT,
+  arg_a7 TEXT,
+  arg_t8 TEXT,
+  arg_c8 TEXT,
+  arg_a8 TEXT,
+  arg_t9 TEXT,
+  arg_c9 TEXT,
+  arg_a9 TEXT,
+  arg_t10 TEXT,
+  arg_c10 TEXT,
+  arg_a10 TEXT,
+  arg_t11 TEXT,
+  arg_c11 TEXT,
+  arg_a11 TEXT,
+  arg_t12 TEXT,
+  arg_c12 TEXT,
+  arg_a12 TEXT,
+  arg_t13 TEXT,
+  arg_c13 TEXT,
+  arg_a13 TEXT,
+  arg_t14 TEXT,
+  arg_c14 TEXT,
+  arg_a14 TEXT,
+  arg_t15 TEXT,
+  arg_c15 TEXT,
+  arg_a15 TEXT,
+  arg_t16 TEXT,
+  arg_c16 TEXT,
+  arg_a16 TEXT,
+  arg_t17 TEXT,
+  arg_c17 TEXT,
+  arg_a17 TEXT,
+  arg_t18 TEXT,
+  arg_c18 TEXT,
+  arg_a18 TEXT,
+  arg_t19 TEXT,
+  arg_c19 TEXT,
+  arg_a19 TEXT,
+  count BIGINT);
+
+CREATE INDEX types_index ON types(
+  count,
+  package_being_analyzed,
+  package,
+  fun_name);
+
+CREATE INDEX main_index ON types(package, fun_name, package_being_analyzed);
+
+CREATE INDEX count_index ON types(count);
+CREATE INDEX analyzed_index ON types(package_being_analyzed);
+CREATE INDEX package_index ON types(package);
+
+--
+-- import from CSV (dropping header)
+--
+
+.mode csv
+.import "2020_04_13_all_very_simpl.csv" types
+DELETE FROM types WHERE rowid = 1;
+
+--
+-- initial data cache tables
+--
+
+CREATE TABLE stats(
+  "name" TEXT,
+  "count" BIGINT
+);
+
+INSERT INTO stats
+SELECT "distinct_package_being_analyzed" as name, COUNT(DISTINCT package_being_analyzed) as count
+FROM types;
+
+INSERT INTO stats
+SELECT "distinct_package" as name, COUNT(DISTINCT package) as count
+FROM types;
+
+INSERT INTO stats
+SELECT "distinct_fun_name" as name, COUNT(DISTINCT fun_name) as count
+FROM types;
+
+
+CREATE TABLE analyzed_packages(
+   "package_being_analyzed" TEXT,
+   "count" BIGINT
+);
+
+INSERT INTO analyzed_packages
+SELECT package_being_analyzed, SUM(count) as count FROM types
+GROUP BY package_being_analyzed
+ORDER BY count DESC;
+
+
+CREATE TABLE init_packages(
+    "package" TEXT, 
+    "count" BIGINT
+);
+
+INSERT INTO init_packages
+SELECT package, SUM(count) as count FROM types
+GROUP BY package
+ORDER BY count DESC;
+
+
+CREATE TABLE init_functions(
+    "fun_name" TEXT,
+    "count" BIGINT
+);
+
+INSERT INTO init_functions
+SELECT fun_name, SUM(count) as count FROM types
+GROUP BY fun_name
+ORDER BY count DESC;
+
+
+CREATE TABLE sums(
+   "package" TEXT, "fun_name" TEXT, "package_being_analyzed" TEXT,
+   "count" BIGINT
+);
+
+INSERT INTO sums
+SELECT package, fun_name, package_being_analyzed, SUM(count) as count FROM types
+GROUP BY package, fun_name, package_being_analyzed
+ORDER BY count DESC;
+
+CREATE INDEX package_sums_index ON sums(package);
+
---
+++ static/js/visualization-new.js
@@ -0,0 +1,284 @@
+// --------------------------------------------------
+// Main Dispatch
+// --------------------------------------------------
+
+((() => {
+
+    // Configuration
+    const INIT_ANALYZED_PACKAGES = [],//["anapuce", "approximator"],
+          INIT_LIMIT = 15,
+          INIT_EXCLUDED_PACKAGES = [], //["base", "foo"],
+          //ROOT_URL = "//69.122.18.134:9898",
+          //ROOT_URL = "//127.0.0.1:5000",
+          ROOT_URL = "http://prl1.ele.fit.cvut.cz:8135/",
+          QUERY_ENDPOINT = ROOT_URL + "/api/query",
+          INIT_QUERY_ENDPOINT = ROOT_URL + "/api/init/query",
+          PACKAGE_ENDPOINT = ROOT_URL + "/api/packages",
+          DEF_NUMS_ENDPOINT = ROOT_URL + "/api/definednums",
+          INIT_DEF_NUMS_ENDPOINT = ROOT_URL + "/api/init/definednums";
+
+    // ----------------------------------------
+    // Constants and global variables
+    // ----------------------------------------
+
+    const initQuery = {
+        package_being_analyzed: INIT_ANALYZED_PACKAGES,
+        excluded: INIT_EXCLUDED_PACKAGES,
+        limit: INIT_LIMIT
+    };
+
+    // global settings shared among treemaps
+    // and main vis
+    let visSettings  = {
+        package_being_analyzed: initQuery.package_being_analyzed,
+        excluded: initQuery.excluded,
+        limit: initQuery.limit,
+        package: [],
+        functions: []
+    };
+
+    // last query used for main diagram
+    let lastMainQuery = deepCopy(initQuery);
+    // last query used for selected package/function name
+    let lastFunctionNameQuery = deepCopy(initQuery);
+    lastFunctionNameQuery.excluded = [];
+
+    // Dispatch
+    let dispatch = d3.dispatch(
+        "push", "pull", "selected-push",
+        "analyzed-push", "analyzed-pull",
+        "funcs-push", "funcs-pull"
+    );
+
+    /// Helper
+    function clean(obj) {
+        return _.pickBy(obj, function(x) {
+            if (_.isArray(x)) { return x.length > 0; }
+            return x;
+        });
+    }
+
+    // ----------------------------------------
+    // Dispatch Logic
+    // ----------------------------------------
+
+    /* Everything starts with analyzed-push,
+       which requests information for selected analyzed packages
+       with the given limit.
+
+       Once the analyzed-push query is processed,
+       analyzed-pull and funcs-pull happen always,
+       and also one of:
+       * push if we are in the "main diagram" mode
+         (i.e. "Keep showing" is not selected or disabled)
+       * selected-push if we are in "Keep showing" mode.
+
+       On analyzed-pull, the package treemap is updated.
+       On funcs-pull, the function treemap and barchart are updated.
+
+       Treemap selections generate push.
+       Package/Function "Show" button generate selected-push.
+     */
+
+    // Query actor
+    {
+        // Initial request, change of limit,
+        // change of excluded packages,
+        // or change of analyzed packages
+        dispatch.on("analyzed-push.query", function(newQuery) {
+            // update settings and last query infos
+            [visSettings, lastFunctionNameQuery, lastMainQuery]
+                .map(q => q.package_being_analyzed = newQuery.package_being_analyzed);
+            // if analyzed packages changed, the main diagram
+            // selection of packages and functions are reset
+            lastMainQuery.package = [];
+            lastMainQuery.functions = [];
+            // request new data
+            let baseEndpoint = _.isEqual(clean(newQuery), clean(initQuery)) ? INIT_QUERY_ENDPOINT : QUERY_ENDPOINT;
+            const endpoint = baseEndpoint + "?" + new URLSearchParams(newQuery);
+            d3.json(endpoint).then(function(data) {
+                dispatch.call("analyzed-pull", this, newQuery, data);
+                dispatch.call("funcs-pull", this, newQuery, data);
+                // depending on what we need to show in types overview,
+                // process either the main diagram or selected package/function
+                if (d3ElemIsChecked(funNameCheckbox)
+                        && !funNameCheckbox.property("disabled"))
+                    dispatch.call("selected-push", this, lastFunctionNameQuery);
+                else
+                    dispatch.call("pull", this, newQuery, data);
+            });
+        });
+
+        // Functions requested for the functions treemap
+        dispatch.on("funcs-push.query", function(newQuery) {
+            //console.log(newQuery);
+            const endpoint = QUERY_ENDPOINT + "?" + new URLSearchParams(newQuery);
+            d3.json(endpoint).then(function(data) {
+                console.log(data);
+                dispatch.call("funcs-pull", this, newQuery, data);
+            });
+        });
+
+        // Someone requested new data for types overview
+        // from the main diagram
+        dispatch.on("push.query", function(newQuery) {
+            lastMainQuery.package = newQuery.package;
+            lastMainQuery.functions = newQuery.functions;
+            let baseEndpoint = _.isEqual(clean(newQuery), clean(initQuery)) ? INIT_QUERY_ENDPOINT : QUERY_ENDPOINT;
+            const endpoint = baseEndpoint + "?" + new URLSearchParams(newQuery);
+            d3.json(endpoint).then(function(data) {
+                dispatch.call("pull", this, newQuery, data);
+            });
+        });
+
+        // Someone requested new data for types overview
+        // from the Package/Function selection
+        dispatch.on("selected-push.query", function(newQuery) {
+            let baseEndpoint = _.isEqual(clean(lastMainQuery), clean(initQuery)) ? INIT_QUERY_ENDPOINT : QUERY_ENDPOINT;
+            const endpoint = baseEndpoint + "?" + new URLSearchParams(lastMainQuery);
+            d3.json(endpoint).then(function(data) {
+                dispatch.call("pull", this, lastMainQuery, data);
+            });
+        });
+    }
+
+    // ----------------------------------------
+    // DOM elements initialization
+    // ----------------------------------------
+
+    // Init vis settings
+    limitText.property("value", INIT_LIMIT);
+    excludedText.property("value", INIT_EXCLUDED_PACKAGES.join("\n"));
+
+    // Analysis info
+    /*d3.json(INIT_DEF_NUMS_ENDPOINT).then(function(data) {
+        //console.log(data);
+        d3.select("#infoDefiningPackagesNum")
+            .text(data[0].packages);
+        d3.select("#infoDefinedFunctionsNum")
+            .text(data[0].functions);
+    });*/
+    d3.json(DEF_NUMS_ENDPOINT).then(function(data) {
+        //console.log(data);
+        d3.select("#infoDefiningPackagesNum")
+            .text(data.packages[0].count);
+        d3.select("#infoDefinedFunctionsNum")
+            .text(data.functions[0].count);
+    });
+
+    funNameText
+        // if function name has changed, invalidate last selection
+        .on("change", function() {
+            //funNameCheckbox.property("checked", false);
+            d3ElemDisable(funNameCheckbox);
+        });
+    // Show
+    funNameButton
+        .on("click", function() {
+            const funNameSelect = getFunctionName();
+            const selectionElems = checkAndGetFunctionNameParams(funNameSelect);
+            //alert(funNameSelect);
+            if (!selectionElems) // bad input
+                return;
+            // enable "Keep showing"
+            d3ElemEnable(funNameCheckbox);
+            const newQuery = deepCopy(visSettings);
+            newQuery.excluded = []; // we don't exclude anything
+            newQuery.package = [selectionElems[0]]; // package is given
+            // function might be given
+            newQuery.functions = (selectionElems.length > 1) ?
+                [selectionElems[1]] : [];
+            // remember last query for function name selection
+            lastFunctionNameQuery = newQuery;
+            console.log(newQuery);
+            // request type overview data
+            dispatch.call("selected-push", this, newQuery);
+        });
+    // Clear
+    funNameClearButton
+        .on("click", function() {
+            // show types overview from the main diagram
+            dispatch.call("push", this, lastMainQuery);
+            d3ElemDisable(funNameCheckbox);
+        });
+
+    // request packages and functions based on the main settings
+    function updateAllData(obj, visSettings) {
+        lastFunctionNameQuery.limit = visSettings.limit;
+        lastMainQuery.limit = visSettings.limit;
+        lastMainQuery.excluded = visSettings.excluded;
+        const newQuery = deepCopy(visSettings);
+        dispatch.call("analyzed-push", obj, newQuery);
+    }
+
+    // Settings updates
+    limitButton
+        .on("click", function() {
+            const newLimit = Number(limitText.property("value"));
+            //alert(newLimit);
+            if (!(Number.isInteger(newLimit)))
+                visERROR("New limit must be an integer");
+            else if (newLimit < 1)
+                visERROR("New limit must be positive");
+            else {
+                visSettings.limit = newLimit;
+                updateAllData(this, visSettings);
+            }
+        });
+    excludedButton
+        .on("click", function() {
+            const newExcluded = excludedText.property("value")
+                .split("\n");
+            visSettings.excluded = newExcluded;
+            updateAllData(this, visSettings);
+        });
+
+    // ----------------------------------------
+    // Initialize Visualization Charts
+    // ----------------------------------------
+
+    // Types overview
+    typesOverviewChart()("#vis-svg-1", dispatch);
+
+    // Analyzed packages
+    d3.json(PACKAGE_ENDPOINT).then(function(data) {
+        //console.log(data);
+        const pkgNames = data.map(
+            d => d.package_being_analyzed
+        );
+        d3.select("#infoAnalyzedPackagesNum")
+            .text(data.length);
+        packageFilter()("#filter", dispatch, data, visSettings);
+        // adding package
+        analyzedPkgButtonSelect
+            .on("click", function() {
+                const pkgName = getAnalyzedPkgName();
+                if (checkAnalyzedPkgName(pkgName, pkgNames)) {
+                    //alert(pkgName);
+                    // select checkbox in the appropriate svg
+                    const pkgCheckBox = d3.select(`#${pkgName}`);
+                    d3ElemMakeChecked(pkgCheckBox);
+                }
+            });
+    });
+
+    dataTreeMap()("#vis-svg-2-pkg-tree-map",
+                  dispatch,
+                  PKGS_LABELS,
+                  PKGS_GETTERS,
+                  "analyzed-pull.pkg-treemap",
+                  PKGS_EVENTS);
+    dataTreeMap()
+        .colorPalette(COLOR_PALETTE_VIOLET)
+        ("#vis-svg-3-fun-tree-map",
+         dispatch,
+         FUNCS_LABELS,
+         FUNCS_GETTERS,
+         "funcs-pull.fun-treemap",
+         FUNCS_EVENTS);
+
+    barChart()("#barchart-1", dispatch);
+
+    // Initial data request
+    updateAllData(this, visSettings);
+})());
